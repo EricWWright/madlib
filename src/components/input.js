f0bbd30dd648f0542e106228a94866d65d5026be
EricWWright <tddragonknight@gmail.com>
2018-07-11 09:39:13 -0600

    added remaining inputs

--- src/components/input.js
+++ src/components/input.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Input = (title, state, onChange, name) => {
+const Input = ({title, state, name}, onChange) => {
     return (
-        <div className="input">
+        <div key={name} className="input">
             <input name={name} value={state} onChange={onChange}/>
-            <lable>{title}</lable>
+            <label>{title}</label>
         </div>
     )
 }
