28be27d796c91f7867fd2ef1f448cf98fc25853f
EricWWright <tddragonknight@gmail.com>
2018-07-16 08:51:26 -0600

    cleaned up the code

--- src/components/home.js
+++ src/components/home.js
@@ -7,16 +7,11 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        { Header() }
+        {Header()}
         <Card />
       </div>
     );
   }
 }
-
-// things we need to fix
-
-// content lables
-// generate btn space/card-height
 
 export default Home;
